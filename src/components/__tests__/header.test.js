25b80c6ae9f000e537bc9edfdeaf8ef10cf06e58
Tuğsan Ünlü <unlutugsan@gmail.com>
2020-08-08 18:23:20 +0300

    Add new test case

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -1,10 +1,23 @@
 import React from "react"
 import renderer from "react-test-renderer"
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
 import Header from "../header"
 
 describe("Header", () => {
   it("Renders check", () => {
     const tree = renderer.create(<Header />).toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  it("Does it contain a title", () => {
+    const siteTitle = "tuğsan ünlü"
+    const siteSubTitle = "front-end developer, cyber security lover"
+    const { getByText } = render(
+      <Header siteTitle={siteTitle} siteSubTitle={siteSubTitle} />
+    )
+    expect(getByText(siteTitle)).toBeInTheDocument()
+    expect(getByText(siteSubTitle)).toBeInTheDocument()
   })
 })
