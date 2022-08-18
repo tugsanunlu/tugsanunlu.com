a683df2a4dc02cac167419e7e0fa3a3b1e083f6b
Tuğsan Ünlü <unlutugsan@gmail.com>
2022-08-18 18:50:06 +0300

    chore: remove legacy files

--- src/utils/uikit.js
+++
@@ -1,8 +0,0 @@
-import UIkit from "uikit"
-
-const offCanvasBeforeShow = () => {
-  UIkit.offcanvas("#offcanvas-push").hide()
-  UIkit.offcanvas("#offcanvas-push").show()
-}
-
-export { offCanvasBeforeShow }
